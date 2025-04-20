657dff7440a705a670a18f2b7a6869cd330c8562
Mensun <mensun@bu.edu>
2025-04-20 04:02:04 -0400

    finished api stuff

---
+++ app/api/getPlayerID/route.ts
@@ -0,0 +1,61 @@
+// Mensun Wang
+"use server"
+import { NextResponse, NextRequest } from 'next/server';
+
+// here's where i got info on this: https://nextjs.org/blog/building-apis-with-nextjs#3-creating-an-api-with-route-handlers
+
+export async function GET(request: NextRequest) {
+  try {
+    // get params data
+    const searchParams = request.nextUrl.searchParams;
+    const name = searchParams.get('name');
+    const tag = searchParams.get('tag');
+
+    // call api
+    const response = await fetch(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/portal/only2`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Riot-Token': `${process.env.RIOT_API}`,
+      },
+    });
+
+    // hit rate limit
+    if (response.status == 429) {
+      return NextResponse.json(
+        { error: 'Rate limit error, please try again another time' },
+        { status: 429 }
+      );
+    }
+
+    // hit not found error
+    if (response.status == 404) {
+      return NextResponse.json(
+        { error: 'Not found error, please enter an existing American name and tag' },
+        { status: 404 }
+      );
+    }
+
+    // if it's not status code 200 or 429 then return error
+    if (response.status != 200) {
+      return NextResponse.json(
+        { error: 'Failed to fetch data, please try again another time' },
+        { status: 500 }
+      );
+    }
+
+    // parse response
+    const data = await response.json();
+
+    // return ID along with success status
+    return NextResponse.json({ id: data.puuid }, { status: 200 });
+
+  } catch (e) {
+    // random or server error here
+    console.error(e);
+    return NextResponse.json(
+      { error: 'Failed to fetch data, please try again another time' },
+      { status: 500 }
+    );
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,50 @@
+'use client'
+import { useEffect } from 'react'
 export default function Home() {
+
+  // gets champion masteries of a player given the name an the tag (temporarily under HOME)
+
+  useEffect(() => {
+    async function getChampionMasteries() {
+      try {
+        // gets ID
+        const name = "portal"
+        const tag = "only2"
+        const query1 = new URLSearchParams({ name, tag }).toString();
+        const response1 = await fetch(`/api/getPlayerID?${query1}`)
+
+        // if status is not 200, something went wrong
+        if (response1.status != 200) {
+          const { error } = await response1.json()
+          console.log(error)
+          throw Error(error)
+        }
+
+        // get their id
+        const { id } = await response1.json()
+
+        // gets their top 10 champion masteries
+        const query2 = new URLSearchParams({ id }).toString();
+        const response2 = await fetch(`/api/getChampionMasteries?${query2}`)
+
+        // if status is not 200, something went wrong
+        if (response2.status != 200) {
+          const { error } = await response2.json()
+          console.log(error)
+          throw Error(error)
+        }
+
+        // get their top 10 champion mastery details
+        const { masteryData } = await response2.json()
+        console.log(masteryData)
+      } catch (e) {
+        console.log("Error:", e)
+      }
+    }
+
+    getChampionMasteries()
+  }, [])
+
   return (
     <>
       hi
